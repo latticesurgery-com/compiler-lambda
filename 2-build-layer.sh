f3652e31aed2e5e4a99104e5b7d9d16c1ca796a9
gwwatkin@sfu.ca <gwwatkin@sfu.ca>
2021-09-20 13:28:29 -0700

    Initial commit: basic lambda scripts from aws example

--- 2-build-layer.sh
+++ 2-build-layer.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-# Copy only the desired files over from the compiler /TODO
-# Clone repo
-#cp lattice-surgery-compiler/*.py function
-#cp lattice-surgery-compiler/requirements.txt function
+# Copy only the desired files over from the compiler
+cp lattice-surgery-compiler/*.py function
+cp lattice-surgery-compiler/requirements.txt function
 
 set -eo pipefail
 rm -rf package
