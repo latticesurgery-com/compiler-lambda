d136ac2674446ec76717d9a3622d393bea208a60
gwwatkin@sfu.ca <gwwatkin@sfu.ca>
2021-10-06 09:09:30 -0700

    Set up cloudformation template

--- 3-deploy.sh
+++ 3-deploy.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
+source config.txt
+
 set -eo pipefail
 ARTIFACT_BUCKET=$(cat bucket-name.txt)
-aws cloudformation package --template-file template.yml --s3-bucket $ARTIFACT_BUCKET --output-template-file out.yml
-aws cloudformation deploy --template-file out.yml --stack-name blank-python --capabilities CAPABILITY_NAMED_IAM
+aws cloudformation package  --template-file template.yml --s3-bucket $ARTIFACT_BUCKET --output-template-file out.yml
+aws  cloudformation deploy  --template-file out.yml --stack-name $STACK_NAME --capabilities CAPABILITY_NAMED_IAM
--- 4-invoke.sh
+++ 4-invoke.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
+source config.txt
+
 set -eo pipefail
-FUNCTION=$(aws cloudformation describe-stack-resource --stack-name blank-python --logical-resource-id function --query 'StackResourceDetail.PhysicalResourceId' --output text)
+FUNCTION=$(aws cloudformation describe-stack-resource --stack-name $STACK_NAME --logical-resource-id CompilerLambda --query 'StackResourceDetail.PhysicalResourceId' --output text)
 
 while true; do
   aws lambda invoke --function-name $FUNCTION --payload file://event.json out.json
