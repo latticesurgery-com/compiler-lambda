a8999276c8d6adf65b83b8f56f0ab870f61397b6
gwwatkin@sfu.ca <gwwatkin@sfu.ca>
2021-10-11 18:33:55 -0700

    Add container config to cloud formation

--- 3-deploy.sh
+++ 3-deploy.sh
@@ -2,6 +2,5 @@
 source config.txt
 
 set -eo pipefail
-ARTIFACT_BUCKET=$(cat bucket-name.txt)
-aws cloudformation package  --template-file template.yml --s3-bucket $ARTIFACT_BUCKET --output-template-file out.yml
-aws  cloudformation deploy  --template-file out.yml --stack-name $STACK_NAME --capabilities CAPABILITY_NAMED_IAM
+
+aws cloudformation deploy --template-file template.yml --stack-name $STACK_NAME --capabilities CAPABILITY_NAMED_IAM --parameter-overrides EcrRepositoryName=$ECR_REPOSITORY_NAME
