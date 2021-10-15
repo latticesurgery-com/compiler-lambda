413be1cdb1b30099dd16320b03f3360052aa3174
gwwatkin@sfu.ca <gwwatkin@sfu.ca>
2021-10-15 13:15:30 -0700

    Fix deploy parameters passed to cloud formation template

--- 3-deploy-stack.sh
+++ 3-deploy-stack.sh
@@ -5,4 +5,4 @@ set -eo pipefail
 
 ACCOUNT_ID=$(aws sts get-caller-identity --query Account --output text)
 
-aws cloudformation deploy --template-file template.yml --stack-name $STACK_NAME --capabilities CAPABILITY_NAMED_IAM --parameter-overrides EcrRepositoryName=$ECR_REPOSITORY_NAME,AccountId=$ACCOUNT_ID
+aws cloudformation deploy --template-file template.yml --stack-name $STACK_NAME --capabilities CAPABILITY_NAMED_IAM --parameter-overrides EcrRepositoryName=$ECR_REPOSITORY_NAME AccountId=$ACCOUNT_ID
