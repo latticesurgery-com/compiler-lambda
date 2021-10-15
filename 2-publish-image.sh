6185596920402a2842ceda620288afd7a3eba4f8
gwwatkin@sfu.ca <gwwatkin@sfu.ca>
2021-10-15 11:59:37 -0700

    Fix lambda existence checking

--- 2-publish-image.sh
+++ 2-publish-image.sh
@@ -15,13 +15,16 @@ if [ -z $CREATED_AT_STR ]; then
     aws ecr create-repository --repository-name $ECR_REPOSITORY_NAME --image-scanning-configuration scanOnPush=true --image-tag-mutability MUTABLE
 fi
 
-docker tag  $IMAGE_NAME:latest $ACCOUNT_ID.dkr.ecr.$REGION.amazonaws.com/$ECR_REPOSITORY_NAME:latest
-docker push $ACCOUNT_ID.dkr.ecr.$REGION.amazonaws.com/$ECR_REPOSITORY_NAME:latest
+
+REPO_LOCATION=$($ACCOUNT_ID.dkr.ecr.$REGION.amazonaws.com/$ECR_REPOSITORY_NAME:latest)
+
+docker tag  $IMAGE_NAME:latest $REPO_LOCATION
+docker push $REPO_LOCATION
 
 
-LAMBDA_NAME= aws lambda list-functions --output text | grep -o -m 1 lattice-surgery-compiler-lambda-CompilerLambda-[a-zA-Z0-9]* | head  -n 1
+LAMBDA_NAME=$(aws lambda list-functions --output text | grep -o -m 1 lattice-surgery-compiler-lambda-CompilerLambda-[a-zA-Z0-9]* | head  -n 1)
 
 if [ ! -z $LAMBDA_NAME ]; then
     echo "Updating the lambda's function code"
-    ws lambda update-function-code --function-name arn:aws:lambda:$REGION:$ACCOUNT_ID:function:$LAMBDA_NAME
+    aws lambda update-function-code --function-name arn:aws:lambda:$REGION:$ACCOUNT_ID:function:$LAMBDA_NAME --image-uri $REPO_LOCATION
 fi
